ca2eddb72547c09e1aacd7192040c8b90b50944b
sweta sengupta <ssengupta801@gmail.com>
2020-10-02 21:59:43 -0500

    finished this section

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
@@ -9,6 +11,20 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
-
+     beg_i = 0
+    end_i = len(arr) - 1
+    
+    found = False
+    while beg_i <= end_i and not found:
+        mid_i = (beg_i + end_i) // 2
+        
+        if arr[mid_i] == target:
+            found = True
+            return mid_i
+        else:
+            if arr[mid_i] > target:
+                end_i = mid_i - 1
+            else:
+                beg_i = mid_i + 1
 
     return -1  # not found
